97e44a6e19294811cde02b8966ce246f9313ea41
Ezard <ezard.ben@gmail.com>
2015-11-26 17:35:55 +0000

    Automatically bring up a dialog for creating/opening a project on startup if the previously opened project can't be determined

--- src/main/java/com/ezardlabs/lostsectormapeditor/Main.java
+++ src/main/java/com/ezardlabs/lostsectormapeditor/Main.java
@@ -2,13 +2,25 @@ package com.ezardlabs.lostsectormapeditor;
 
 import com.ezardlabs.lostsectormapeditor.gui.GUI;
 
+import java.io.File;
+import java.util.prefs.Preferences;
+
 public class Main {
+	private GUI gui;
 
 	public static void main(String[] args) {
 		new Main();
 	}
 
 	public Main() {
-		new GUI();
+		Preferences prefs = Preferences.userNodeForPackage(Main.class);
+		String temp;
+		File f;
+		gui = new GUI();
+		if ((temp = prefs.get("project_current", null)) != null && (f = new File(temp)).exists() && f.isDirectory()) {
+			// load project
+		} else {
+			gui.showNewOrOpenDialog();
+		}
 	}
 }

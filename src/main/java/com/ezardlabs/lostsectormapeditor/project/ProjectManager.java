c04536bb8162d9a1df76e0368e5989e91da49c33
Ezard <ezard.ben@gmail.com>
2015-12-07 18:43:47 +0000

    Made use of the file system watcher to sync up changes with the project tree

--- src/main/java/com/ezardlabs/lostsectormapeditor/project/ProjectManager.java
+++ src/main/java/com/ezardlabs/lostsectormapeditor/project/ProjectManager.java
@@ -2,17 +2,17 @@ package com.ezardlabs.lostsectormapeditor.project;
 
 import com.ezardlabs.lostsectormapeditor.Main;
 import com.ezardlabs.lostsectormapeditor.gui.ProjectPanel;
+import com.ezardlabs.lostsectormapeditor.project.DirectoryWatcher.FileChangeListener;
 import com.google.gson.Gson;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.WatchService;
 import java.util.prefs.Preferences;
+
+;
 
 public class ProjectManager {
 	private static Project currentProject;
@@ -34,10 +34,26 @@ public class ProjectManager {
 		if (projectFile.exists()) {
 			try {
 				currentProject = new Gson().fromJson(new FileReader(projectFile), Project.class);
-			} catch (FileNotFoundException ignored) {
+				projectPanel.setProject(currentProject);
+				Preferences.userNodeForPackage(Main.class).put("project_current", directory.getAbsolutePath());
+				new DirectoryWatcher(directory, new FileChangeListener() {
+					@Override
+					public void onFileCreated(File file) {
+						projectPanel.setProject(currentProject);
+
+					}
+
+					@Override
+					public void onFileModified(File file) {
+					}
+
+					@Override
+					public void onFileDeleted(File file) {
+						projectPanel.setProject(currentProject);
+					}
+				}).processEvents();
+			} catch (IOException ignored) {
 			}
-			projectPanel.setProject(currentProject);
-			Preferences.userNodeForPackage(Main.class).put("project_current", directory.getAbsolutePath());
 
 		}
 	}

bbb29156d224e4d52a5fdc4980c45e774c57a88b
Ezard <ezard.ben@gmail.com>
2016-03-24 17:29:06 +0000

    Started work on user-editable details for sprites

---
+++ src/main/java/com/ezardlabs/lostsectormapeditor/sprites/SpriteDetailsPanel.java
@@ -0,0 +1,60 @@
+package com.ezardlabs.lostsectormapeditor.sprites;
+
+import com.ezardlabs.lostsectormapeditor.gui.Panel;
+
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class SpriteDetailsPanel extends Panel {
+
+	public SpriteDetailsPanel() {
+		SpringLayout layout = new SpringLayout();
+		setLayout(layout);
+
+		JLabel nameLabel = new JLabel("Name:");
+		add(nameLabel);
+		JTextField nameField = new JTextField();
+		add(nameField);
+
+		JLabel indexLabel = new JLabel("Index:");
+		add(indexLabel);
+		JLabel indexField = new JLabel("0");
+		add(indexField);
+
+		JLabel colliderLabel = new JLabel("Collider:");
+		add(colliderLabel);
+		JCheckBox colliderField = new JCheckBox();
+		add(colliderField);
+
+		// Name label
+		layout.putConstraint(SpringLayout.NORTH, nameLabel, 15, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.WEST, nameLabel, 15, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.SOUTH, nameLabel, 0, SpringLayout.SOUTH, nameField);
+
+		// Name field
+		layout.putConstraint(SpringLayout.NORTH, nameField, 15, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.WEST, nameField, 15, SpringLayout.EAST, nameLabel);
+		layout.putConstraint(SpringLayout.EAST, nameField, -15, SpringLayout.EAST, this);
+
+		// Index label
+		layout.putConstraint(SpringLayout.NORTH, indexLabel, 15, SpringLayout.SOUTH, nameLabel);
+		layout.putConstraint(SpringLayout.WEST, indexLabel, 15, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.SOUTH, indexLabel, 0, SpringLayout.SOUTH, indexField);
+
+		// Index field
+		layout.putConstraint(SpringLayout.NORTH, indexField, 15, SpringLayout.SOUTH, nameField);
+		layout.putConstraint(SpringLayout.WEST, indexField, 15, SpringLayout.EAST, indexLabel);
+		layout.putConstraint(SpringLayout.EAST, indexField, -15, SpringLayout.EAST, this);
+
+		// Collider label
+		layout.putConstraint(SpringLayout.NORTH, colliderLabel, 15, SpringLayout.SOUTH, indexLabel);
+		layout.putConstraint(SpringLayout.WEST, colliderLabel, 15, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.SOUTH, colliderLabel, 0, SpringLayout.SOUTH, colliderField);
+
+		// Collider field
+		layout.putConstraint(SpringLayout.NORTH, colliderField, 15, SpringLayout.SOUTH, indexField);
+		layout.putConstraint(SpringLayout.EAST, colliderField, -15, SpringLayout.EAST, this);
+	}
+}
--- src/main/java/com/ezardlabs/lostsectormapeditor/sprites/SpriteManager.java
+++ src/main/java/com/ezardlabs/lostsectormapeditor/sprites/SpriteManager.java
@@ -7,18 +7,22 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.swing.JFrame;
-import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 
 public class SpriteManager {
 
 	public static void showSpriteCutterDialog(File file) {
 		JFrame frame = new JFrame();
+
+		SpriteDetailsPanel sdp = new SpriteDetailsPanel();
 		SpriteCutterPanel scp = new SpriteCutterPanel((int) (GUI.getInstance().getWidth() * 0.8), (int) (GUI.getInstance().getHeight() * 0.8));
+		scp.setPreferredSize(new Dimension((int) (GUI.getInstance().getWidth() * 0.6), (int) (GUI.getInstance().getHeight() * 0.8)));
 		try {
 			scp.loadSpritesheet(file);
 		} catch (IOException ignored) {
 		}
-		frame.setContentPane(new JScrollPane(scp));
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scp, sdp);
+		frame.add(splitPane);
 		frame.setPreferredSize(new Dimension((int) (GUI.getInstance().getWidth() * 0.8), (int) (GUI.getInstance().getHeight() * 0.8)));
 		frame.setResizable(false);
 		frame.pack();
